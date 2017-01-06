15e1bfaf62bea1dd76308ddaeb32f95cf7bec24c
KolyaAlavackii <nikolayalavackii@gmail.com>
2017-01-06 23:06:31 +0200

    готово

--- src/js/car.js
+++ src/js/car.js
@@ -1,1 +1,43 @@
 ﻿'use strict';
+
+ var car = {
+    color: "white",
+    dours: 4,
+    speed: 0,
+    defaultSpeed: 60,
+    maxSpeed: 100,
+    seats: 4,
+    passengers: [],
+    put: function() {
+        if (this.passengers.length < this.seats) {
+            this.passengers.push(true);
+        }
+        else if (this.passengers.length > this.seats) {
+            this.passengers.pop();
+        }
+    },
+
+    land: function() {
+        this.passengers.pop();
+    },
+
+    drive: function(newSpeed) {
+        this.speed = newSpeed;
+        if (this.speed === undefined) {
+            this.speed = this.defaultSpeed;
+        }
+        else if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        else if (this.passengers.length === 0) {
+            this.speed = 0;
+        }
+    }
+ };
+
+module.exports = car;
+ 
+        
+        
+
+
